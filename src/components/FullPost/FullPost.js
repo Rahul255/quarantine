0e0305c78d3afff01034e1422948c87d172b1b81
Rahul255 <rahulmohan255@gmail.com>
2020-05-09 00:30:23 +0530

    file added

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+        loadedPost: null
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id).then(response => {
+            console.log(response);
+        });
+    }
+
     render () {
-        let post = <p>Please select a Post!</p>;
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        if (this.props.id) {
+
         post = (
             <div className="FullPost">
                 <h1>Title</h1>
@@ -15,6 +27,7 @@ class FullPost extends Component {
             </div>
 
         );
+    }
         return post;
     }
 }
