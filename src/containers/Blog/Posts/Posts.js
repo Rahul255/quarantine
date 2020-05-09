d1bc3670aeffb73acbf9fdf1c22f69b19888b4df
Rahul255 <rahulmohan255@gmail.com>
2020-05-09 23:52:36 +0530

    file added

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -9,6 +9,7 @@ class Posts extends  Component{
         error: false
     }
     componentDidMount() {
+        console.log(this.props);
         axios.get('/posts').then(response => {
             const posts = response.data.slice(0, 4);
             const updatePosts = posts.map(post => {
